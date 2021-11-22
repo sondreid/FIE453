70a7a3be43b48caa3724d25412332c32dbbc5edf
Olai <olaiviken@icloud.com>
2021-11-22 18:21:43 +0100

    Remove NA function

--- Final Paper/preprocessing.R
+++ Final Paper/preprocessing.R
@@ -2,14 +2,20 @@
 ########################### Pre-processing #####################################
 ################################################################################
 
-# Libraris
+# Libraries --------------------------------------------------------------------
 library(tidyverse)
 library(magrittr)
 
-# Set WD
+
+
+
+# Set WD -----------------------------------------------------------------------
 setwd("~/OneDrive - Norges Handelshøyskole/MASTER/FIE453/FinalExam/FIE453/Final Paper")
 
-# Load and read data
+
+
+
+# Load and read data -----------------------------------------------------------
 load("data/merged.Rdata")
 company_names_df <- read.csv(file = "data/names.csv")
 feature_names_df <- read.delim(file = "data/compustat-fields.txt")    
@@ -19,27 +25,64 @@ merged %<>% rename_with(tolower)
 
 
 
-# Subset of 100 companies
-permno <- merged %>% 
-    select(PERMNO) %>% 
+
+# Subset of 100 companies ------------------------------------------------------
+permno_top <- (merged %>% 
+    select(permno) %>% 
     unique() %>% 
-    head(1000)
+    head(100))$permno
+
+df <- merged %>% filter(permno %in% permno_top)
 
 
-remove_zero_and_NA <- function(df, ratio) {
+# Feature selection functions --------------------------------------------------
+remove_cols_only_zero_and_NA <- function(df, print_removed_cols = F) {
+    
+    #'@description Function that removes columns containing only zeros and NAs
     #'
-    #'@ratio: ratio of NA's which a column of data cannot exceed
+    #'@param df    Passing a data frame
+    #'@param print_removed_cols True if user want to print removed columns
+    #'@return      Data frame without columns that containing only zeros and NAs
+
+    cols <- df %>% apply(MARGIN = 2, function(x) (sum(x==0, na.rm = T) + sum(is.na(x)))/length(x))
+    cols <- cols[cols == 1] %>% as.data.frame() %>% rownames() 
     
-    out_df <- df 
-    df[is.na(df)] <- 0
-    cols <- df %>% apply(MARGIN = 2, function(x) sum(x==0, na.rm = T)/length(x)) 
-    cols <- cols[cols < ratio] %>% as.data.frame() %>% rownames() 
+    if(print_removed_cols) cat("Columns removed: ", cols, "\n")
     
-    return (
-        out_df %>% select(cols)
-    )
+    return (out_df %>% select(-cols))
 }
-test_df <- df %>% remove_zero_and_NA(0.5)
+
+
+
+
+remove_NA <- function(df, ratio, print_removed_cols = F){
+    
+    #'@description Function that removes columns containing NAs beyond a given
+    #'             ratio
+    #'             
+    #'@param df    Passing a data frame
+    #'@param ratio Passing a upper limit NA ratio
+    #'@param print_removed_cols True if user want to print removed columns
+    #'@return      Data frame without columns containing NAs beyond given ratio
+    
+    cols <- df %>% apply(MARGIN = 2, function(x) sum(is.na(x))/length(x))
+    cols <- cols[cols >= ratio] %>% as.data.frame() %>% rownames() 
+    
+    if(print_removed_cols) cat("Columns removed: ", cols, "\n")
+    
+    return(df %>% select(-cols))
+}
+
+
+
+
+
+test_df <- df %>% remove_cols_only_zero_and_NA(T) %>% remove_NA(0.2, T)
+
+
+
+
+
 
 ###### EITHER FULLY MERGED SET OR A REDUCED DATASET
 
@@ -58,15 +101,6 @@ df_reduced <- merged %>%
 merged %<>% 
     remove_all_zero_columns() %>%
     remove_all_duplicates()
-
-
-
-### Subset selection
-
-
-
-subset_of_variabels <-  regsubsets(RETX~., df_reduced )
-summary(subset_of_variabels)
 
 
 
