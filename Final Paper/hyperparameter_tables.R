2943b16e54d9bc68997318e04f1bed7a87349de8
sondreid <sondre.eide@gmail.com>
2021-12-13 17:03:11 +0100

    Final parameters of optimal nn model: table

--- Final Paper/hyperparameter_tables.R
+++ Final Paper/hyperparameter_tables.R
@@ -28,9 +28,17 @@ tibble("Neural network models" = list("One hidden layer", "Two hidden layers", "
 
 
 tibble("Number of layers" = list(2),
-       "Learning rates" = list(list(0.005, 0.1, 0.4), c(0.005, 0.1, 0.4),list(0.005, 0.1, 0.4), list(0.005, 0.1, 0.4)),
-       "Batch normalization" = list(list("No"), list("Yes", "No"), list("Yes", "No"), list("Yes", "No")),
-       "Dropout rates" = list(list(0), list(0, 0.2, 0.4), list(0, 0.2, 0.4), list(0, 0.2, 0.4)),
-       "Batch sizes"   = list(list(300, 500, 1000), list(500, 1000, 2000), list(500, 1000, 2000), list(500, 1000, 2000)),
-       "Maximum epochs" 
-) 
+       "Learning rates" = 0.005,
+       "Batch normalization" = "No",
+       "Dropout rates" = 0,
+       "Batch sizes"   = 500,
+       "Patience" =  20) %>% 
+  t() %>% 
+  kable(caption = "Best Performing Neural Network Hyperparameters", 
+        digits  = 4) %>% 
+  kable_classic(full_width = F, 
+                html_font  = "Times New Roman") %>% 
+  save_kable("images/neural_network_optimal_parameters.png", 
+             zoom = 3, 
+             density = 1900)
+
