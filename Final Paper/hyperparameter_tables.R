7ea60c7b25da299abab2acd9ea27922d1265df83
Jon Liane <joon.liane@gmail.com>
2021-12-13 17:45:27 +0100

    La til tabell med feature description

--- Final Paper/hyperparameter_tables.R
+++ Final Paper/hyperparameter_tables.R
@@ -1,7 +1,7 @@
 
 ################################### HYPEPARAMETER TABLES #########################
-
-
+library(tidyr)
+library(kableExtra)
 
 ## Neural network tested hyperparameters
 tibble("Neural network models" = list("One hidden layer", "Two hidden layers", "Three hidden layers", "Four hiden layers"),
@@ -16,5 +16,24 @@ tibble("Neural network models" = list("One hidden layer", "Two hidden layers", "
   kable_classic(full_width = F, 
                 html_font  = "Times New Roman")  %>% 
   save_kable("images/neural_network_tested_parameters.png", 
+             zoom = 3, 
+             density = 1900)
+
+
+
+tibble("vwretd" = "Market Excess Return", "vol" = "Volume", "shrout" = "Shared Outstanding", "cshfdq" = "Common Shared for Diluted EPS", 
+       "aoq" = "Assets - Other - Total", "cheq" = "Cash and Short-Term Investments",
+       "cogsq" = "Cost of Goods Sold", "apq" = "Account Payable/Creditors - Trade", "epsfxq" = "Earnings Per Share (Diluted) - Excluding Extraodinary Items", 
+       "chq" = "Cash", "citotalq" = "Comprehensive Income - Parent", "ceqq" = "Common/Ordinary Equity - Total",
+       "dpq" = "Depreciation and Amortization - Total", "lcoq" = "Current Liabilities - Other - Total",
+       "capsq" = "Capital Surplus/Share Premium Reserve", "acoq" = "Current Asets - Other - Total",
+       "invtq" = "Inventories - Total", "cstkq" = "Common/Ordinary Stock (Capital)",
+       "intanoq" = "Other Intangibles", "dlttq" = "Long-Term Debt - Total") %>% 
+  t() %>% 
+  kable(caption = "Feature Description", 
+        digits  = 4) %>% 
+  kable_classic(full_width = F, 
+                html_font  = "Times New Roman")  %>% 
+  save_kable("C:/Users/joonl/Downloads/feature_description1.png", 
              zoom = 3, 
              density = 1900)
